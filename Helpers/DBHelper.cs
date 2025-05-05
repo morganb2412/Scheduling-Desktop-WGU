ea1b495e252bb451a54b2a6b2573da9462ebc804
Morgan <77501297+morganb2412@users.noreply.github.com>
2025-05-05 15:13:28 -0500

    Update DBHelper.cs

--- Helpers/DBHelper.cs
+++ Helpers/DBHelper.cs
@@ -4,7 +4,7 @@ namespace SchedulingDesktopWGU.Helpers
 {
     public static class DBHelper
     {
-        private const string connectionString = "server=localhost;user id=root;password=Cruz072724$;database=client_schedule;";
+        private const string connectionString = "server=localhost;user id=sqlUser;password=Passw0rd!;database=client_schedule;";
         public static MySqlConnection conn = new MySqlConnection(connectionString);
 
         public static void OpenConnection()
